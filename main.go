47ee0cf6fb464e6512a0d8a4cdb2a957b0809e49
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-02-13 11:52:58 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Key struct {
+	User   string `json:"user"`
+	PubKey string `json:"pubkey"`
+}
+
+var keys []Key
+
+func main() {
+	router := mux.NewRouter()
+	// call with:
+	//  curl -H "Content-Type: application/json" -d '{"user":"hey","pubkey":"hoooo"}' -X POST http://127.0.0.1:8400/key
+	router.HandleFunc("/key", AddKey).Methods("POST")
+	router.HandleFunc("/key/{userkey}", GetKey).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8400", router))
+}
+
+func AddKey(w http.ResponseWriter, r *http.Request) {
+	var newkey Key
+	_ = json.NewDecoder(r.Body).Decode(&newkey)
+	// TODO: check error
+	WriteKey(newkey.User, newkey.PubKey)
+
+}
+
+func WriteKey(user string, key string) {
+	err := ioutil.WriteFile(user+".pub", []byte(key), 0644)
+	if err != nil {
+		fmt.Printf("Writing to File failed with error %s\n", err)
+	}
+
+}
+
+func GetKey(w http.ResponseWriter, r *http.Request) {
+
+}
