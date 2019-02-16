17e56c8393b23dac679ca84087a4698c4203452b
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-02-16 10:49:13 +0100

    uploading key

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	//	"os"
 )
 
 type Key struct {
@@ -22,6 +26,10 @@ func main() {
 	//  curl -H "Content-Type: application/json" -d '{"user":"hey","pubkey":"hoooo"}' -X POST http://127.0.0.1:8400/key
 	router.HandleFunc("/health", Health).Methods("GET")
 	router.HandleFunc("/key", AddKey).Methods("POST")
+	router.
+		Path("/keyfile").
+		Methods("POST").
+		HandlerFunc(AddKeyFile)
 	router.HandleFunc("/key/", GetKey).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8400", router))
 }
@@ -35,7 +43,30 @@ func AddKey(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&newkey)
 	// TODO: check error
 	WriteKey(newkey.User, newkey.PubKey)
+}
 
+func AddKeyFile(w http.ResponseWriter, r *http.Request) {
+	//https://stackoverflow.com/questions/40684307/how-can-i-receive-an-uploaded-file-using-a-golang-net-http-server
+	var Buf bytes.Buffer
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	name := strings.Split(header.Filename, ".")
+	// Copy the file data to my buffer
+	io.Copy(&Buf, file)
+	// do something with the contents...
+	// I normally have a struct defined and unmarshal into a struct, but this will
+	// work as an example
+	contents := Buf.String()
+	fmt.Println(contents)
+	// I reset the buffer in case I want to use it again
+	// reduces memory allocations in more intense projects
+	Buf.Reset()
+	// do something else
+	// etc write header
+	return
 }
 
 func WriteKey(user string, key string) {
