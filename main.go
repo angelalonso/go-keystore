ab78b3dd98e6e25ea80e37ab1d655c676c32ab91
Ángel Alonso Fonseca <angel.fonseca@.deliveryhero.com>
2019-02-26 13:14:17 +0100

    commenting main.go

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ type Key struct {
 	User   string `json:"user"`
 	PubKey string `json:"pubkey"`
 }
-
+// Variables used all across the application
 var a App
 
 var keys []Key
 
+// main is just the initialization of the API server
 func main() {
 	a = App{}
 	a.Initialize()
